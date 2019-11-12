e64de28ccee90199ec100caf26fa78665db11370
nathanclarke@deloitte.co.uk <nclarke732@outlook.com>
2019-11-12 15:58:28 +0000

    added classes

--- amazon-s3/src/main/java/com/amazon/s3bucket/build/Main.java
+++ amazon-s3/src/main/java/com/amazon/s3bucket/build/Main.java
@@ -6,8 +6,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World 1");
         new Build();
-        System.out.println("Hello World 2");
     }
 }
--- amazon-s3/src/main/java/com/amazon/s3bucket/function/Build.java
+++ amazon-s3/src/main/java/com/amazon/s3bucket/function/Build.java
@@ -11,7 +11,7 @@ public class Build
     {
         Scanner scan = new Scanner(System.in);
         try {
-            System.out.println("Please enter your accessKey" + "Please enter your secretKey");
+            System.out.println("Please enter your accessKey\n" + "Please enter your secretKey");
             conn = new Connection(scan.nextLine(), scan.nextLine());
             bo = new BucketOperation(conn);
             oo = new ObjectOperation(conn);
@@ -71,15 +71,15 @@ public class Build
             boolean run = true;
 
             System.out.println(
-                    "MENU OPTIONS" +
-                            "-------------------" +
-                            "0 : System Exit." +
-                            "1 : Main Menu" +
-                            "2 : Create Bucket" +
-                            "3 : Delete Bucket" +
-                            "4 : View List of All Buckets" +
-                            "-------------------" +
-                            "Enter an option from the menu.");
+                "MENU OPTIONS\n" +
+                "-------------------\n" +
+                "0 : System Exit.\n" +
+                "1 : Main Menu\n" +
+                "2 : Create Bucket\n" +
+                "3 : Delete Bucket\n" +
+                "4 : View List of All Buckets\n" +
+                "-------------------\n" +
+                "Enter an option from the menu.\n");
 
             while (run == true)
             {
@@ -121,17 +121,17 @@ public class Build
             boolean run = true;
 
             System.out.println(
-                "MENU OPTIONS" +
-                "-------------------" +
-                "0 : System Exit." +
-                "1 : Main Menu" +
-                "2 : List all the available objects in S3 bucket" +
-                "3 : Upload File to Bucket" +
-                "4 : Download File from Bucket" +
-                "5 : Copy Object to another Bucket" +
-                "6 : Delete Object" +
-                "-------------------" +
-                "Enter an option from the menu.");
+                "MENU OPTIONS\n" +
+                "-------------------\n" +
+                "0 : System Exit.\n" +
+                "1 : Main Menu\n" +
+                "2 : List all the available objects in S3 bucket\n" +
+                "3 : Upload File to Bucket\n" +
+                "4 : Download File from Bucket\n" +
+                "5 : Copy Object to another Bucket\n" +
+                "6 : Delete Object\n" +
+                "-------------------\n" +
+                "Enter an option from the menu.\n");
 
             while (run == true)
             {
@@ -144,33 +144,33 @@ public class Build
                         displayMainMenu();
                         break;
                     case 2:
-                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From.");
+                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From.\n");
                         oo.listObjects(scan.nextLine());
                         break;
                     case 3:
-                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From."
-                                + "Enter Your Buckets Key."
-                                + "Enter The Path You Would Like The File To Be Uploaded.");
+                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From.\n"
+                                + "Enter Your Buckets Key.\n"
+                                + "Enter The Path You Would Like The File To Be Uploaded.\n");
                         oo.uploadObject(scan.nextLine(), scan.nextLine(), scan.nextLine());
                         break;
                     case 4:
-                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From."
-                                + "Enter Your Buckets Key."
-                                + "Enter The Path You Would Like The File To Be Downloaded.");
+                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From.\n"
+                                + "Enter Your Buckets Key.\n"
+                                + "Enter The Path You Would Like The File To Be Downloaded.\n");
                         oo.downloadObject(scan.nextLine(), scan.nextLine(), scan.nextLine());
                         break;
                     case 5:
-                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From."
-                                + "Enter Your Buckets Key."
-                                + "Please Enter The Bucket Name You Want To Send The Data To."
-                                + "Enter Your Buckets Key.");
+                        System.out.println("Please Enter The Bucket Name You Want To Retrieve The Data From.\n"
+                                + "Enter Your Buckets Key.\n"
+                                + "Please Enter The Bucket Name You Want To Send The Data To.\n"
+                                + "Enter Your Buckets Key.\n");
                         oo.copyObjectToBucket(scan.nextLine(), scan.nextLine(), scan.nextLine(), scan.nextLine());
                     case 6:
-                        System.out.println("Please Enter The Bucket Name You Want To Delete The Data From."
-                                        + "Enter Your Buckets Key.");
+                        System.out.println("Please Enter The Bucket Name You Want To Delete The Data From.\n"
+                                        + "Enter Your Buckets Key.\n");
                         oo.deleteObject(scan.nextLine(), scan.nextLine());
                     default:
-                        System.out.println("Selection is not on menu, please try again or enter 0 to exit program.");
+                        System.out.println("Selection is not on menu, please try again or enter 0 to exit program.\n");
                         displayBucketOperationMenu();
                         break;
                 }
